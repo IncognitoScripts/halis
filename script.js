2c70fecbc7890259aa7553e5d612889124ff8863
IncognitoScripts <luka.jijavadzebrawl@gmail.com>
2025-06-10 17:37:26 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const title = document.querySelector('.title')
+    const text = 'ჰალისის ბრძოლა – 1609'
+    let i = 0
+    function type() {
+        if (i < text.length) {
+            title.innerHTML += text.charAt(i)
+            i++
+            setTimeout(type, 100)
+        }
+    }
+    type()
+})
